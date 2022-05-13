af81f9242893109abe890b13e18e772bbc5b23ae
Luoxi Pan <l.panpax@gmail.com>
2022-05-13 22:27:46 +1000

    Replace hardcoded object with argument

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ function addBookToLibrary(bookObject) {
     myLibrary.push(bookObject);
 }
 
-function createTableHeader() {
+function createTableHeader(referenceObject) {
     let tableHead = document.createElement('thead');
     let tableRow = document.createElement('tr');
     
-    Object.keys(theHobbit).forEach(key => {
+    Object.keys(referenceObject).forEach(key => {
         let headerCell = document.createElement('th');
         headerCell.innerText = convertToTitleCase(key);
         tableRow.appendChild(headerCell);
@@ -35,22 +35,25 @@ function createTableHeader() {
     return tableHead;
 }
 
-theHobbit = new Book('The Hobbit', 'JRR Tolkien', 500, false);
-harryPotter = new Book('Harry Potter', 'JK Rowling', 499, false);
-nineteenEightyFour = new Book('1984', 'George Orwell', '384', true);
+function populateLibrary() {
+    theHobbit = new Book('The Hobbit', 'JRR Tolkien', 500, false);
+    harryPotter = new Book('Harry Potter', 'JK Rowling', 499, false);
+    nineteenEightyFour = new Book('1984', 'George Orwell', '384', true);
 
-
-addBookToLibrary(theHobbit);
-addBookToLibrary(harryPotter);
-addBookToLibrary(nineteenEightyFour);
+    addBookToLibrary(theHobbit);
+    addBookToLibrary(harryPotter);
+    addBookToLibrary(nineteenEightyFour);
+}
 
 // loop through library and display book info
 // myLibrary.forEach(book => console.log(book.info()));
 
+populateLibrary();
+
 let body = document.querySelector('body');
 let table = document.createElement('table');
 
-table.appendChild(createTableHeader());
+table.appendChild(createTableHeader(myLibrary[0]));
 body.appendChild(table);
 
 
